f5baa7bb908314aa8275c5ef983251963e6a94cb
leogutschow <73674285+leogutschow@users.noreply.github.com>
2022-01-18 19:58:00 -0300

    Testando Bootstrap no Front-End da Home Page

--- littlelink/settings.py
+++ littlelink/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -121,11 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILE_DIRS = [
-    BASE_DIR / 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'templates/static/'
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Crispy
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
