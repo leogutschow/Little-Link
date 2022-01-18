d1dce1cb17877fe9bfdbd94c458ad88ca2d02112
leogutschow <73674285+leogutschow@users.noreply.github.com>
2022-01-18 18:04:56 -0300

    Configurando Arquivos Staticos

--- littlelink/settings.py
+++ littlelink/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILE_DIRS = [
+    BASE_DIR / 'static/'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
